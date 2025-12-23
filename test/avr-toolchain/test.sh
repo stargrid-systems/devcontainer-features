ce8e5be8d5c70c12bfc44f821270739a2c7809ff
Simon Berger <simon@siku2.io>
2025-12-23 01:20:37 +0100

    feat: add avr-toolchain feature

---
+++ test/avr-toolchain/test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# See https://github.com/devcontainers/cli/blob/HEAD/docs/features/test.md#dev-container-features-test-lib
+# Provides the 'check' and 'reportResults' commands.
+source dev-container-features-test-lib
+
+check 'avrdude is installed' avrdude --version
+check 'ravedude is installed' ravedude --version
+
+reportResults
