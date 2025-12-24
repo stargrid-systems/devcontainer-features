b543a96e152233dcd906062e5fb0c6d9832fbd24
Simon Berger <simon@siku2.io>
2025-12-24 01:06:13 +0100

    test: ensure library.sh is actually installed

--- test/base/test.sh
+++ test/base/test.sh
@@ -7,5 +7,7 @@ source dev-container-features-test-lib
 
 check 'cargo-binstall is installed' cargo-binstall -V
 check 'shellhistory onCreate script configures HISTFILE for bash' grep 'export HISTFILE=/mnt/shellhistory/.bash_history' "$HOME/.bashrc"
+check 'library.sh is present' test -f /usr/local/share/devcontainer-features/base/library.sh
+check 'library.sh can be sourced' bash -c 'source /usr/local/share/devcontainer-features/base/library.sh'
 
 reportResults
