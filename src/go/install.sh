c07b07f5a6adf50815ba6979f8189e57bbfd124c
Simon Berger <simon@siku2.io>
2025-12-24 01:10:54 +0100

    feat: add golang feature

---
+++ src/go/install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# shellcheck source=../base/library.sh
+source /usr/local/share/devcontainer-features/base/library.sh
+
+APT_PACKAGES=(
+    'golang'
+)
+
+main() {
+    base__apt_install "${APT_PACKAGES[@]}"
+}
+
+main "$@"
---
+++ test/go/test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# See https://github.com/devcontainers/cli/blob/HEAD/docs/features/test.md#dev-container-features-test-lib
+# Provides the 'check' and 'reportResults' commands.
+source dev-container-features-test-lib
+
+check 'go is installed' go version
+
+reportResults
