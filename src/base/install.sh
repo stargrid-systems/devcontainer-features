1f68bcca67b549f86e51bbafbc8f37b5ce64b9da
Simon Berger <simon@siku2.io>
2025-12-27 22:47:43 +0100

    refactor: remove user creation from base feature

--- src/base/install.sh
+++ src/base/install.sh
@@ -9,45 +9,23 @@ TARGET_SCRIPTS_DIR='/usr/local/share/devcontainer-features/base'
 # renovate: datasource=github-releases depName=cargo-binstall packageName=cargo-bins/cargo-binstall versioning=semver
 CARGO_BINSTALL_VERSION=1.16.5
 APT_PACKAGES=(
-    'bash-completion'
     'ca-certificates'
     'curl'
     'git'
     'gnupg'
     'make'
     'rustup'
-    'sudo'
-    'zsh'
 )
 
 install_cargo_binstall() {
-    export BINSTALL_VERSION="${CARGO_BINSTALL_VERSION}"
-    export CARGO_HOME='/usr/local'
-    curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+    declare -x BINSTALL_VERSION="${CARGO_BINSTALL_VERSION}"
+    declare -x CARGO_HOME='/usr/local'
+    curl -L -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 }
 
 install_scripts() {
     mkdir -p "${TARGET_SCRIPTS_DIR}"
     cp library.sh oncreate.sh "${TARGET_SCRIPTS_DIR}"
-}
-
-prepare_dev_user() {
-    if [ "${REMOTE_USER}" = "root" ]; then
-        echo >&2 "REMOTE_USER must not be 'root'."
-        exit 1
-    fi
-
-    # Create the user
-    useradd \
-        --home-dir "$_REMOTE_USER_HOME" \
-        --gid "$_REMOTE_USER" \
-        --groups 'sudo' \
-        --create-home \
-        --shell '/usr/bin/zsh' \
-        "$_REMOTE_USER"
-    # Allow passwordless sudo for the user
-    echo "$_REMOTE_USER ALL=\(root\) NOPASSWD:ALL" >"/etc/sudoers.d/$_REMOTE_USER-sudo"
-    chmod 0440 "/etc/sudoers.d/$_REMOTE_USER-sudo"
 }
 
 main() {
@@ -58,7 +36,6 @@ main() {
     apt-get autoremove -y
 
     install_cargo_binstall
-    prepare_dev_user
 }
 
 main "$@"
