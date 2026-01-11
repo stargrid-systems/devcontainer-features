3b9d17d082762d9c477613cd4e1fc40e7469e10c
Simon <simon@siku2.io>
2026-01-11 21:03:14 +0100

    feat: create markdown feature (#92)
    * feat: create markdown feature
    
    * fup

---
+++ test/markdown/test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# See https://github.com/devcontainers/cli/blob/HEAD/docs/features/test.md#dev-container-features-test-lib
+# Provides the 'check' and 'reportResults' commands.
+source dev-container-features-test-lib
+
+check 'markdownlint-cli2 is installed' markdownlint-cli2 --version
+
+reportResults
